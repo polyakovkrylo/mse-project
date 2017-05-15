346801289d4a7c5962541571d468f26b641ec4f1
Hector Gerardo Munoz Hernandez <nec_ge99@hotmail.com>
2017-05-15 20:13:50 +0200

    STM32F3discovery code servo & gyroscope working

---
+++ eclipse_code/Glider/.dep/hal_lld.o.d
@@ -0,0 +1,301 @@
+build/obj/hal_lld.o: \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/STM32F3xx/hal_lld.c \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/hal.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/osal/rt/osal.h \
+ /opt/gcc-arm-none-eabi/lib/gcc/arm-none-eabi/5.4.1/include/stddef.h \
+ /opt/gcc-arm-none-eabi/lib/gcc/arm-none-eabi/5.4.1/include/stdint.h \
+ /opt/gcc-arm-none-eabi/arm-none-eabi/include/stdint.h \
+ /opt/gcc-arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
+ /opt/gcc-arm-none-eabi/arm-none-eabi/include/sys/features.h \
+ /opt/gcc-arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
+ /opt/gcc-arm-none-eabi/arm-none-eabi/include/sys/_intsup.h \
+ /opt/gcc-arm-none-eabi/arm-none-eabi/include/sys/_stdint.h \
+ /opt/gcc-arm-none-eabi/lib/gcc/arm-none-eabi/5.4.1/include/stdbool.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/ch.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/ports/ARMCMx/compilers/GCC/chtypes.h \
+ chconf.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chlicense.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chsystypes.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/ports/ARMCMx/chcore.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/common/ports/ARMCMx/devices/STM32F3xx/cmparams.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/boards/ST_STM32F3_DISCOVERY/board.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/ext/CMSIS/ST/STM32F3xx/stm32f3xx.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/ext/CMSIS/ST/STM32F3xx/stm32f303xc.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/ext/CMSIS/include/core_cm4.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/ext/CMSIS/include/core_cmInstr.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/ext/CMSIS/include/core_cmFunc.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/ext/CMSIS/include/core_cmSimd.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/ext/CMSIS/ST/STM32F3xx/system_stm32f3xx.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/ports/ARMCMx/chcore_v7m.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/ports/ARMCMx/chcore_timer.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/st.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/TIMv1/st_lld.h \
+ mcuconf.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/STM32F3xx/stm32_registry.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/TIMv1/stm32_tim.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chdebug.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chtm.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chstats.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chschd.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chsys.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chvt.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chthreads.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chregistry.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chsem.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chbsem.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chmtx.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chcond.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chevents.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chmsg.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chmboxes.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chmemcore.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chheap.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chmempools.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chdynamic.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chqueues.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chstreams.h \
+ halconf.h mcuconf.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/STM32F3xx/hal_lld.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/STM32F3xx/stm32_registry.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/common/ARMCMx/nvic.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/STM32F3xx/stm32_isr.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/DMAv1/stm32_dma.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/STM32F3xx/stm32_rcc.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/hal_streams.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/hal_channels.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/hal_files.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/hal_ioblock.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/hal_mmcsd.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/hal_buffers.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/hal_queues.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/pal.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/GPIOv2/pal_lld.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/adc.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/can.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/dac.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/ext.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/gpt.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/i2c.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/I2Cv2/i2c_lld.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/i2s.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/icu.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/TIMv1/icu_lld.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/mac.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/mii.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/pwm.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/TIMv1/pwm_lld.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/rtc.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/serial.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/USARTv2/serial_lld.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/sdc.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/spi.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/SPIv2/spi_lld.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/uart.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/usb.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/USBv1/usb_lld.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/USBv1/stm32_usb.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/wdg.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/st.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/mmc_spi.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/serial_usb.h \
+ /home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/usb_cdc.h
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/hal.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/osal/rt/osal.h:
+
+/opt/gcc-arm-none-eabi/lib/gcc/arm-none-eabi/5.4.1/include/stddef.h:
+
+/opt/gcc-arm-none-eabi/lib/gcc/arm-none-eabi/5.4.1/include/stdint.h:
+
+/opt/gcc-arm-none-eabi/arm-none-eabi/include/stdint.h:
+
+/opt/gcc-arm-none-eabi/arm-none-eabi/include/machine/_default_types.h:
+
+/opt/gcc-arm-none-eabi/arm-none-eabi/include/sys/features.h:
+
+/opt/gcc-arm-none-eabi/arm-none-eabi/include/_newlib_version.h:
+
+/opt/gcc-arm-none-eabi/arm-none-eabi/include/sys/_intsup.h:
+
+/opt/gcc-arm-none-eabi/arm-none-eabi/include/sys/_stdint.h:
+
+/opt/gcc-arm-none-eabi/lib/gcc/arm-none-eabi/5.4.1/include/stdbool.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/ch.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/ports/ARMCMx/compilers/GCC/chtypes.h:
+
+chconf.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chlicense.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chsystypes.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/ports/ARMCMx/chcore.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/common/ports/ARMCMx/devices/STM32F3xx/cmparams.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/boards/ST_STM32F3_DISCOVERY/board.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/ext/CMSIS/ST/STM32F3xx/stm32f3xx.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/ext/CMSIS/ST/STM32F3xx/stm32f303xc.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/ext/CMSIS/include/core_cm4.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/ext/CMSIS/include/core_cmInstr.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/ext/CMSIS/include/core_cmFunc.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/ext/CMSIS/include/core_cmSimd.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/ext/CMSIS/ST/STM32F3xx/system_stm32f3xx.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/ports/ARMCMx/chcore_v7m.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/ports/ARMCMx/chcore_timer.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/st.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/TIMv1/st_lld.h:
+
+mcuconf.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/STM32F3xx/stm32_registry.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/TIMv1/stm32_tim.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chdebug.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chtm.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chstats.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chschd.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chsys.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chvt.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chthreads.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chregistry.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chsem.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chbsem.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chmtx.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chcond.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chevents.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chmsg.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chmboxes.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chmemcore.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chheap.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chmempools.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chdynamic.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chqueues.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/rt/include/chstreams.h:
+
+halconf.h:
+
+mcuconf.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/STM32F3xx/hal_lld.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/STM32F3xx/stm32_registry.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/common/ARMCMx/nvic.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/STM32F3xx/stm32_isr.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/DMAv1/stm32_dma.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/STM32F3xx/stm32_rcc.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/hal_streams.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/hal_channels.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/hal_files.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/hal_ioblock.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/hal_mmcsd.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/hal_buffers.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/hal_queues.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/pal.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/GPIOv2/pal_lld.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/adc.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/can.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/dac.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/ext.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/gpt.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/i2c.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/I2Cv2/i2c_lld.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/i2s.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/icu.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/TIMv1/icu_lld.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/mac.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/mii.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/pwm.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/TIMv1/pwm_lld.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/rtc.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/serial.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/USARTv2/serial_lld.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/sdc.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/spi.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/SPIv2/spi_lld.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/uart.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/usb.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/USBv1/usb_lld.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/ports/STM32/LLD/USBv1/stm32_usb.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/wdg.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/st.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/mmc_spi.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/serial_usb.h:
+
+/home/hector/Programs/eclipse/ChibiOS_16.1.7/os/hal/include/usb_cdc.h:
